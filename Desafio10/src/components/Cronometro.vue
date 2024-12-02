4a7116fedbb04d27240c691a55e633478750fd8f
felipe mascena <felipe.mascena.dev@gmail.com>
2024-12-02 13:09:42 -0300

    iniciando o cronômetro

--- Desafio10/src/components/Cronometro.vue
+++ Desafio10/src/components/Cronometro.vue
@@ -1,5 +1,71 @@
 <script setup>
+import { onMounted, onUnmounted, ref } from 'vue';
+
+// Configurando a data limite
+const targetDate = new Date('2024-12-25T00:00:00');
+
+//Refs para os dados
+const days = ref(0);
+const hours = ref(0);
+const minutes = ref(0);
+const seconds = ref(0);
+
+// Função para atualizar o cronômetro
+const updateTimer = () => {
+  const now = new Date();
+  const timeLeft = targetDate - now;
+
+  if (timeLeft > 0) {
+    days.value = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    hours.value = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
+    minutes.value = Math.floor((timeLeft / (1000 * 60)) % 60);
+    seconds.value = Math.floor((timeLeft / 1000) % 60);
+  } else {
+    days.value = hours.value = minutes.value = seconds.value = 0;
+    clearInterval(timer);
+  }
+};
+
+// Atualizando os segundos do cronômetro
+let timer;
+onMounted(() => {
+  updateTimer();
+  timer = setInterval(updateTimer, 1000);
+});
+
+// limpando o intervalo
+onUnmounted(() => {
+  clearInterval(timer);
+});
 </script>
 
 <template>
+  <main>
+    <section>
+      <div>
+        <h2>Tempo limitado</h2>
+        <p>
+          Nessas festas de fim de ano mande um presente para a pessoa amada e
+          compartilhe a alegria do Natal
+        </p>
+        <p>
+            <span>{{ days }}d</span> -
+            <span>{{ hours }}</span> -
+            <span>{{ minutes }}</span> -
+            <span>{{ seconds }}</span>
+        </p>
+        <img src="../assets/presentes.png" alt="imagem de presentes de Natal">
+      </div>
+      <div>
+        <figure>
+            <img src="../assets/decoracao.png" alt="Colocando decoração">
+        </figure>
+        <div>
+            <h2>Conectando generosidade ao redor do mundo</h2>
+            <p>Celebre a magia do Natal de uma maneira especial! Descubra a plataforma que permite a doação de presentes significativos para aqueles que mais precisam. Faça parte do movimento e compartilhe o verdadeiro espírito da generosidade.</p>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
+<style lang="scss"></style>
--- Desafio10/src/components/Inicio.vue
+++ Desafio10/src/components/Inicio.vue
@@ -3,27 +3,85 @@ import logo from '../assets/logo2.svg';
 </script>
 
 <template>
-  <section>
-    <div>
-      <img :src="logo" alt="logo com feliz natal" />
-      <h1>Uma <span>plataforma de doação</span> para espalhar alegria</h1>
-      <p>
-        Nessas festas de fim de ano mande um presente para a pessoa amada e
-        compartilhe a alegria do Natal.
-      </p>
-      <button>Fazer uma doação</button>
-    </div>
-    <div>
-      <img src="../assets/loja.png" alt="imagem de loja" />
-    </div>
-  </section>
+  <header>
+    <section class="section-inicio">
+      <div class="inicio">
+        <img :src="logo" alt="logo com feliz natal" />
+        <h1>Uma <span>plataforma de doação</span> para espalhar alegria</h1>
+        <p>
+          Nessas festas de fim de ano mande um presente para a pessoa amada e
+          compartilhe a alegria do Natal.
+        </p>
+        <button class="btn">Fazer uma doação</button>
+      </div>
+      <div class="loja">
+        <img src="../assets/loja.png" alt="imagem de loja" />
+      </div>
+    </section>
+  </header>
 </template>
 <style scoped lang="scss">
-section {
+header {
   display: flex;
-  justify-content: space-between;
   align-items: center;
-  height: 100vh;
-  background: linear-gradient(180deg, #f7f9fc 0%, #e5e9f2 100%);
+  justify-content: center;
+  background-color: #fac8c5;
+  padding: 3rem 5rem;
+}
+
+.section-inicio {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 90%;
+}
+
+.inicio {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 50%;
+  gap: 1.5rem;
+
+  h1 {
+    font-size: 1.8rem;
+  }
+
+  img {
+    width: 25%;
+  }
+
+  span {
+    color: rgb(205, 60, 50);
+  }
+
+  p {
+    font-size: 1.4rem;
+  }
+
+  .btn {
+    background-color: rgb(205, 60, 50);
+    color: white;
+    border: none;
+    width: 40%;
+    padding: 0.6rem 1.2rem;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+
+    &:hover {
+      background-color: rgb(255, 0, 0);
+      text-decoration: underline;
+      box-shadow: 1px 1px 10px 1px #000;
+    }
+  }
+}
+
+.loja {
+  display: flex;
+  justify-content: center;
+
+  img {
+    width: 90%;
+  }
 }
 </style>
