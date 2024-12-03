e04c425deae024cb9875180e02c3771c9c72e89d
felipe mascena <felipe.mascena.dev@gmail.com>
2024-12-02 23:29:20 -0300

    iniciando desafio 02

---
+++ Desafio02/src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header>
+            <p>Frete grátis para todo o Brasil</p>
+        </header>
+    )
+}
--- Desafio02/vite.config.js
+++ Desafio02/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // Permite que o servidor aceite conexões externas
+    port: 3000, // (Opcional) Define a porta que será usada
+  },
+});
